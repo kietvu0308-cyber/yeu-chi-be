111a03c9a65032bf3dd427321c4046bfc0cba900
kietvu0308-cyber <kietvu0308@gmail.com>
2026-05-26 08:57:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,17 +10,16 @@ const MAX_IMAGES = 5;
 let play = true;
 let noCount = 0;
 
-// Danh sách link ảnh GIF mới từ Imgur - Đảm bảo hiển thị 100%
+// Đường dẫn gọi trực tiếp file ảnh GIF trong kho chứa GitHub của bạn
 const noGifs = [
-  "https://i.imgur.com/vH3LIdA.gif",     // Lần 1: Khóc ăn vạ
-  "https://i.imgur.com/pZqNAnX.gif",     // Lần 2: Buồn thiu
-  "https://i.imgur.com/S9gXNsc.gif",     // Lần 3: Đập bàn dỗi
-  "https://i.imgur.com/mOtwmR8.gif",     // Lần 4: Quay lưng giận dữ
-  "https://i.imgur.com/K6qYvYh.gif"      // Lần 5: Chắp tay cầu xin van nài
+  "cat-1.gif",
+  "cat-2.gif",
+  "cat-3.gif",
+  "cat-4.gif",
+  "cat-5.gif"
 ];
 
-// Ảnh khi cô ấy bấm "Em chấp nhận" (Mèo nhảy hạnh phúc)
-const yesGif = "https://i.imgur.com/z7bZInw.gif";
+const yesGif = "cat-yes.gif";
 
 yesButton.addEventListener("click", handleYesClick);
 
@@ -29,7 +28,6 @@ noButton.addEventListener("click", function () {
     noCount++;
     const imageIndex = Math.min(noCount, MAX_IMAGES);
     
-    // Đổi ảnh hoạt hình đáng yêu tương ứng theo mức độ bấm No
     if (noCount <= MAX_IMAGES) {
       catImg.src = noGifs[imageIndex - 1];
     }
@@ -46,7 +44,7 @@ noButton.addEventListener("click", function () {
 function handleYesClick() {
   titleElement.innerHTML = "Anh iu bé ,Anh hứa hongg làm bé buồn nữa đouuu :3 ❤️";
   buttonsContainer.classList.add("hidden");
-  catImg.src = yesGif; // Hiện ảnh mèo nhảy ăn mừng
+  catImg.src = yesGif;
 }
 
 function resizeYesButton() {
@@ -54,7 +52,6 @@ function resizeYesButton() {
   const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
   let newFontSize = fontSize * 1.5;
 
-  // Giới hạn kích thước tối đa để nút đồng ý không to tràn quá màn hình làm mất thẩm mỹ
   if (newFontSize > 80) {
     newFontSize = 80;
   }
